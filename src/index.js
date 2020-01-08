9edc6257c424c05736e6380e751e6f228a8f5c6f
unknown <narizhnyi01@gmail.com>
2020-01-08 17:59:49 +0200

    refactoring state

--- src/index.js
+++ src/index.js
@@ -1,23 +1,26 @@
 import * as serviceWorker from './serviceWorker';
-import state, {subscribe} from "./redux/state";
+import store from "./redux/state";
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import {addMessage, addPost, updateNewPostText} from "./redux/state";
 import {BrowserRouter} from "react-router-dom";
 
 
 let renderEntireTree = (state) => {
     ReactDOM.render(
         <BrowserRouter>
-            <App state={state} addMessage={addMessage}  addPost={addPost} updateNewPostText={updateNewPostText}/>
+            <App state={state}
+                 addMessage={store.addMessage.bind(store)}
+                 addPost={store.addPost.bind(store)}
+                 updateNewPostText={store.updateNewPostText.bind(store)}/>
         </BrowserRouter>, document.getElementById('root')
     );
 }
 
-renderEntireTree(state);
-subscribe(renderEntireTree);
+renderEntireTree(store.getState());
+
+store.subscribe(renderEntireTree);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,69 +1,83 @@
-let renderEntireTree = () => {
-    console.log('state changed');
-}
-let state = {
-    profilePage:{
-        posts:[
-            {id: 1, message: 'hello', like: '12'},
-            {id: 2, message: 'how are you?', like: '17'},
-            {id: 2, message: 'how are you?', like: '27'},
-            {id: 2, message: 'how are 21?', like: '37'},
-            {id: 2, message: 'how are you111?', like: '47'}
-        ],
-        newPostText: ''
+let store = {
+    _state: {
+        profilePage:{
+            posts:[
+                {id: 1, message: 'hello', like: '12'},
+                {id: 2, message: 'how are you?', like: '17'},
+                {id: 2, message: 'how are you?', like: '27'},
+                {id: 2, message: 'how are 21?', like: '37'},
+                {id: 2, message: 'how are you111?', like: '47'}
+            ],
+            newPostText: ''
+        },
+
+        messagesPage:{
+            dialogs:[
+                {id: 1, name: 'Slava'},
+                {id: 2, name: 'Anton'},
+                {id: 3, name: 'Alona'},
+                {id: 4, name: 'July'},
+                {id: 5, name: 'Ruslan'}
+            ],
+            messages:[
+                {id: 1, message: 'hello'},
+                {id: 2, message: 'how are you?'},
+                {id: 3, message: 'fine'},
+                {id: 4, message: 'yo'}
+
+            ]
+        },
+        navbar:{
+            fiends:[
+
+                {link: 'https://golos.ua/images/items/2019-10/05/2Ax0dEeeGDrEodk9/img_top.jpg'},
+                {link: 'https://golos.ua/images/items/2019-10/05/2Ax0dEeeGDrEodk9/img_top.jpg'},
+                {link: 'https://golos.ua/images/items/2019-10/05/2Ax0dEeeGDrEodk9/img_top.jpg'}
+            ]
+        }
+
     },
+    getState() {
 
-    messagesPage:{
-        dialogs:[
-            {id: 1, name: 'Slava'},
-            {id: 2, name: 'Anton'},
-            {id: 3, name: 'Alona'},
-            {id: 4, name: 'July'},
-            {id: 5, name: 'Ruslan'}
-        ],
-        messages:[
-            {id: 1, message: 'hello'},
-            {id: 2, message: 'how are you?'},
-            {id: 3, message: 'fine'},
-            {id: 4, message: 'yo'}
-
-        ]
+        return this._state;
     },
-    navbar:{
-        fiends:[
+    _callSubscriber() {
+        console.log('state changed');
+    },
+    addPost() {
 
-            {link: 'https://golos.ua/images/items/2019-10/05/2Ax0dEeeGDrEodk9/img_top.jpg'},
-            {link: 'https://golos.ua/images/items/2019-10/05/2Ax0dEeeGDrEodk9/img_top.jpg'},
-            {link: 'https://golos.ua/images/items/2019-10/05/2Ax0dEeeGDrEodk9/img_top.jpg'}
-        ]
+
+        let newPost = {
+            id: 5,
+            message: this._state.profilePage.newPostText,
+            like: 0
+        };
+        this._state.profilePage.posts.push(newPost);
+        this._state.profilePage.newPostText = '';
+        this._callSubscriber(this._state);
+    },
+    updateNewPostText(newText) {
+        this._state.profilePage.newPostText = newText;
+        this._callSubscriber(this._state);
+    },
+    addMessage(dialogMessage) {
+        let newMessage = {
+            id: 1,
+            message: dialogMessage
+        };
+        this._state.messagesPage.messages.push(newMessage);
+        this._callSubscriber(this._state);
+    },
+    subscribe(observer) {
+        this._callSubscriber = observer;
     }
 
+
+
 }
 
-export const addPost = () => {
-    let newPost = {
-        id: 5,
-        message: state.profilePage.newPostText,
-        like: 0
-    };
-    state.profilePage.posts.push(newPost);
-    state.profilePage.newPostText = '';
-    renderEntireTree(state);
-}
-export const updateNewPostText = (newText) => {
-    state.profilePage.newPostText = newText;
-    renderEntireTree(state);
-}
-export const addMessage = (dialogMessage) => {
-    let newMessage = {
-        id: 1,
-        message: dialogMessage
-    };
-    state.messagesPage.messages.push(newMessage);
-    renderEntireTree(state);
-}
-export const subscribe = (observer) =>{
-    renderEntireTree = observer;
-}
 
-export default state;
+
+
+export default store;
+window.store = store;
