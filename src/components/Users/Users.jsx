fbc3685913c8f25e745e457ec8ce1c5a7a2faa56
unknown <narizhnyi01@gmail.com>
2020-03-02 22:07:20 +0200

    add get request from server

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,26 +1,21 @@
-import React from "react"
-import style from "./Style/styleUsers.module.css"
+import React from "react";
+import style from "./Style/styleUsers.module.css";
+import * as axios from "axios";
+import userPhoto from "../../images/icon.png";
+
 
 let Users = (props) => {
-
     if( props.users.length === 0){
-        props.setUsers(
-            [
-                {id: 1, urlPhoto: 'https://www.aviatextile.com/upload/iblock/1ca/%D0%9F%D0%B8%D0%BB%D0%BE%D1%82_%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80_%D0%B0%D0%B2%D0%B8%D0%B0%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C_2.png',
-                    followed: false, fullName: 'Viacheslav', status: 'I am learn React', location:{ country: 'Ukraine', city: 'Kharkiv'} },
-                {id: 2, urlPhoto: 'https://www.aviatextile.com/upload/iblock/1ca/%D0%9F%D0%B8%D0%BB%D0%BE%D1%82_%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80_%D0%B0%D0%B2%D0%B8%D0%B0%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C_2.png',
-                    followed: false, fullName: 'Dmitry', status: 'I am learn React too', location:{ country: 'Russia', city: 'Moscow'} },
-                {id: 3, urlPhoto: 'https://www.aviatextile.com/upload/iblock/1ca/%D0%9F%D0%B8%D0%BB%D0%BE%D1%82_%D0%B0%D0%B2%D0%B0%D1%82%D0%B0%D1%80_%D0%B0%D0%B2%D0%B8%D0%B0%D1%82%D0%B5%D0%BA%D1%81%D1%82%D0%B8%D0%BB%D1%8C_2.png',
-                    followed: true, fullName: 'Anton', status: 'I am learn React too', location:{ country: 'Belarus', city: 'Minsk'} }
-            ]
-        )
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then( response => {
+            props.setUsers(response.data.items);
+        });
     }
     return (
         <div>
             {
-                props.users.map(u => <div className={style.row}>
+                props.users.map(u => <div key={u.id} className={style.row}>
                     <div className={style.col_1}>
-                        <img src={u.urlPhoto} alt=""/>
+                        <img src={ u.photos.small != null ? u.photos.small : userPhoto} alt=""/>
                         { u.followed
                             ? <button onClick={ () => {props.unfollow(u.id)} }>unfollow</button>
                             : <button onClick={ () => {props.follow(u.id)} }>follow</button>}
@@ -28,12 +23,12 @@ let Users = (props) => {
                     </div>
                     <div className={style.row}>
                         <span>
-                            <div className="name">{u.fullName}</div>
+                            <div className="name">{u.name}</div>
                             <div className="status">{u.status}</div>
                         </span>
                         <span>
-                           <div>{u.location.country}</div>
-                           <div>{u.location.city}</div>
+                           <div>{"u.location.country"}</div>
+                           <div>{"u.location.city"}</div>
                        </span>
 
                     </div>
