6d0de999070d701a9b94f5a14698716f4cd3499f
unknown <narizhnyi01@gmail.com>
2020-06-08 12:13:28 +0300

    update app

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import style from './ProfileInfo.module.css';
 import Preloader from "../../Common/Preloadrer/Preloader";
+import ProfileStatus from "./ProfileStatus";
 
 
 const ProfileInfo = (props) => {
@@ -11,10 +12,11 @@ const ProfileInfo = (props) => {
 	return (
 	<div>
 		<div className={style.banner_image}>
-			<img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt=''/>
+			{/*<img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500' alt=''/>*/}
 		</div>
 		<div className={style.body_post}>
 			<img src={props.profile.photos.large} alt=""/>
+			<ProfileStatus status={'hello my'}/>
 			<div>Меня зовут: {props.profile.fullName}</div>
 			<div>Немного обо мне: {props.profile.aboutMe}</div>
 			<div>поиск работы: {props.profile.lookingForAJobDescription}</div>
